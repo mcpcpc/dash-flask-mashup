7529c39d879b65a99221876dc8c9ff25fd8fbd26
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-02-04 10:49:12 -0500

    update

--- pages/home.py
+++ pages/home.py
@@ -4,9 +4,12 @@
 from dash import register_page
 
 from dash_mantine_components import Container
+from dash_mantine_components import Text
+from dash_mantine_components import Title
 
 layout = Container(
 	children=[
+		Title("Getting Started", order=1),
 		
 	]
 )
