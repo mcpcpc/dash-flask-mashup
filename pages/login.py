a72f98cc4c5cb673ea823e0a0f1ea9a9c53c7390
Michael Czigler <37268479+mcpcpc@users.noreply.github.com>
2023-02-05 13:21:10 -0500

    update

--- pages/login.py
+++ pages/login.py
@@ -8,21 +8,35 @@ from dash_mantine_components import Text
 from dash_mantine_components import Title
 from dash_mantine_components import PasswordInput
 from dash_mantine_components import TextInput
+from dash_mantine_components import Button
+from dash_mantine_components import Group
 
 layout = Container(
 	children=[
 		Title("Log In", order=1, weight=400),
-		Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus euismod sem lorem, in fermentum magna auctor eget. Duis molestie dignissim ullamcorper. Aliquam cursus felis in lobortis auctor."),
+		Text("Welcome back! Please provide valid credentials to access additional tools."),
 		TextInput(
+			id="username",
 			placeholder="Username",
 		),
 		PasswordInput(
-			label="Password",
-			description="Password must include at least one letter, number and special character",
+			id="password",
 			placeholder="Password",
-			required=True,
-			error=True,
 		),
+		Group(
+			children=[
+				Button(
+					id="submit",
+					children="Log In",
+					variant="light"
+				),
+				Button(
+					id="cancel",
+					children="Cancel",
+					variant="subtle"
+				),
+			]	
+		)
 	]
 )
 
